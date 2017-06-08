3ae802ca03f7433eb523ec3ee2206d26a04c9fd0
otaviocals <otaviolcals@gmail.com>
2017-06-08 20:54:50 -0300

    test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -12,7 +12,7 @@ const todos = [
     },
     {
         _id: new ObjectID(),
-        text: "Secocnd test todo"
+        text: "Second test todo"
     }];
 
 beforeEach((done) =>
@@ -20,7 +20,7 @@ beforeEach((done) =>
         Todo.remove({}).then(() =>
             {
                 Todo.insertMany(todos);
-            }).then(() => done());
+            }).then(() => done() );
 
     });
 
@@ -119,6 +119,51 @@ describe("GET /todos/:id", () =>
             {
                 request(app)
                     .get("/todos/123")
+                    .expect(404)
+                    .end(done);
+            });
+    });
+
+describe("DELETE /todos/:id", () =>
+    {
+        it("should remove a todo", (done) =>
+            {
+                var hexId = todos[1]._id.toHexString();
+
+                request(app)
+                    .delete("/todos/"+hexId)
+                    .expect(200)
+                    .expect((res) =>
+                        {
+                            expect(res.body.todo._id).toBe(hexId);
+                        })
+                        .end((err, res) =>
+                            {
+                                if(err)
+                                {
+                                    return done(err);
+                                }
+
+                                Todo.findById(hexId).then((todo) =>
+                                    {
+                                        expect(todo).toNotExist();
+                                        done();
+                                    }).catch((e) => done(e) );
+                            });
+            });
+
+        it("should return 404 if todo not found", (done) =>
+            {
+                request(app)
+                    .delete("/todos/"+new ObjectID().toHexString())
+                    .expect(404)
+                    .end(done);
+            });
+
+        it("should return 404 if object id is invalid", (done) =>
+            {
+                request(app)
+                    .delete("/todos/123")
                     .expect(404)
                     .end(done);
             });
