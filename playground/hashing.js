4230a991e21c35da24c63551e9519db589a2e2ea
otaviocals <otaviolcals@gmail.com>
2017-06-10 21:53:49 -0300

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,34 @@
 const {SHA512} = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-var data =
+var password = "123abc!";
+
+bcrypt.genSalt(10, (err, salt) =>
     {
-        id: 10
-    };
+        bcrypt.hash(password, salt, (err, hash) =>
+            {
+                console.log(hash);
+            });
+    });
 
-var token = jwt.sign(data,"123abc");
-console.log(token);
+var hashedPassword = "$2a$10$g5ShWS3sdiXKoxDY7cOqbOsr9OsBLtXlsPghQSE5JhgVFyXvefVuG";
 
-var decoded = jwt.verify(token, "123abc");
-console.log("Decoded: ", decoded);
+bcrypt.compare(password, hashedPassword, (err, res) =>
+    {
+        console.log(res);
+    });
+
+//var data =
+//    {
+//        id: 10
+//    };
+
+//var token = jwt.sign(data,"123abc");
+//console.log(token);
+//
+//var decoded = jwt.verify(token, "123abc");
+//console.log("Decoded: ", decoded);
 
 //var message = "I am user number 3";
 //var hash = SHA512(message).toString();
